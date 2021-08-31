16b1e9f926d9ffeacf3c4016dab12f1e59e010cd
bybit0408 <bybit0408@gmail.com>
2021-08-31 15:57:33 +0900

    building 'a job scrapper' done

--- webscrapping/indeed.py
+++ webscrapping/indeed.py
@@ -5,7 +5,7 @@ LIMIT = 50
 URL = f"https://kr.indeed.com/취업?q=python&limit={LIMIT}"
 
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
 
     soup = BeautifulSoup(result.text, "html.parser")
@@ -54,7 +54,7 @@ def extract_job(html):
     }
 
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
@@ -66,3 +66,8 @@ def extract_indeed_jobs(last_page):
         job = extract_job(result)
         jobs.append(job)
     return jobs
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page) 
+  return jobs
