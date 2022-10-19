422e4ae98d979bcd6d328490c080c4289f007282
byulcode <bybit0408@gmail.com>
2022-10-19 09:07:26 +0900

    # 스택의 활용 - 백준3986 완료

---
+++ algorithm/Stack/3986.py
@@ -0,0 +1,19 @@
+# 백준 3986 좋은 단어
+from sys import stdin
+
+N = int(stdin.readline().rstrip())
+cnt = 0
+for i in range(N):
+    word = stdin.readline().rstrip()
+    stack = []
+    for a in word:
+        if stack:
+            if stack[-1] == a:
+                stack.pop()
+            else:
+                stack.append(a)
+        else:
+            stack.append(a)
+    if not stack:
+        cnt += 1
+print(cnt)
